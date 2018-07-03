414e52d29c4a16a26f87e6b8cc6bfeaf983a794f
jd <juandiegogarita@gmail.com>
2018-07-02 21:33:38 -0600

    includes a temporary unit test

---
+++ test/arrayTest.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+});
