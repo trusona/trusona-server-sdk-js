ebbc7295a101049b2807362a3678b185d38187f5
jd <juandiegogarita@gmail.com>
2018-07-12 19:29:46 -0600

    includes implementation for the HmacSignatureGenerator

--- src/hmacSignatureGenerator.js
+++ src/hmacSignatureGenerator.js
@@ -1,6 +1,24 @@
+var crypto = require("crypto");
 class HmacSignatureGenerator {
   getSignature(hmacMessage, secret) {
-    return 'tacos'
+    const message = hmacMessage.getHmacMessage();
+    
+    const parts = [
+      message.method,
+      message.bodyDigest,
+      message.contentType,
+      message.date,
+      message.requestUri
+    ] 
+
+    var valueToDigest = parts.join(`\n`);
+    const hash = crypto.createHmac('sha256', secret)
+                   .update(valueToDigest)
+                   .digest('hex');
+
+  
+    return Buffer.from(hash).toString('base64');
+
   }
 }
 
