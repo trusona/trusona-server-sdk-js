32cbe99ea6f64506cd93cd2911724680ec4c7442
jd <juandiegogarita@gmail.com>
2018-08-20 15:24:53 -0600

    Includes new methods to handle documents and deactivate users

--- src/trusona.js
+++ src/trusona.js
@@ -56,6 +56,45 @@ class Trusona {
 
     return request(options);
   }
+
+  deactivateUser(userIdentifier){
+    const options = this.requestHelper.getSignedRequest({
+      url: `https://api.staging.trusona.net/api/v2/users/${userIdentifier}`,
+      method: 'DELETE' });
+
+    return request(options);
+  }
+
+  getIdentityDocument(document_id) {
+    const options = this.requestHelper.getSignedRequest({
+      url: `https://api.staging.trusona.net/api/v2/identity_documents/${document_id}`,
+      method: 'GET',
+      transform : (body, response, resolveWithFullResponse) => {
+        body.active = body.is_active;
+        return body;
+      }
+    });
+
+    return request(options);
+  }
+
+  findIdentityDocuments(userIdentifier) {
+    const options = this.requestHelper.getSignedRequest({
+      url: `https://api.staging.trusona.net/api/v2/identity_documents/`,
+      method: 'GET',
+      transform : (body, response, resolveWithFullResponse) => {
+        body.active = body.is_active;
+        return body;
+      },
+      query: userIdentifier
+    });
+
+    return request(options);
+  }
+  
+
+  
+  
 }
 
 module.exports = Trusona
