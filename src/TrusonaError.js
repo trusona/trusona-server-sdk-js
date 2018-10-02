39a525897b9af66720f6b6cfb4ec70eebdc86e9a
jd <juandiegogarita@gmail.com>
2018-10-01 20:48:34 -0600

    Removes uneeded comment

--- src/TrusonaError.js
+++ src/TrusonaError.js
@@ -3,7 +3,6 @@ class TrusonaError extends Error {
     constructor(message) {
         super(message);
         this.name = this.constructor.name;
-       // Error.captureStackTrace(this, this.constructor); -> Check with Ryan to confirm if this option is needed.
     }
 }
 
