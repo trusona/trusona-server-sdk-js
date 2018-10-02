7300669859581d12819ce9313f8a1f8569f331fc
jd <juandiegogarita@gmail.com>
2018-10-01 20:48:56 -0600

    Removes unneeded constructor methods

--- src/DeviceNotFoundError.js
+++ src/DeviceNotFoundError.js
@@ -1,11 +1,6 @@
 const TrusonaError = require('./TrusonaError')
 
 class DeviceNotFoundError extends TrusonaError {
-
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-    }
 }
 
 module.exports = DeviceNotFoundError
