bf135dadc793356ee6db2cfd1de649183816645d
jd <juandiegogarita@gmail.com>
2018-10-11 21:09:15 -0600

    Includes user error handler

---
+++ src/UserErrorHandler.js
@@ -0,0 +1,16 @@
+const GenericErrorHandler = require('./GenericErrorHandler')
+const UserNotFoundError = require('./UserNotFoundError')
+
+class UserErrorHandler extends GenericErrorHandler {
+
+    static handleError(error){
+        super.handleError(error)
+        switch(error.statusCode) {
+            case 404:
+                throw new UserNotFoundError(error.statusCode + " - " +
+                    "The user you are attempting to deactivate does not exist or is already inactive.");
+        }
+    }
+}
+
+module.exports = UserErrorHandler
