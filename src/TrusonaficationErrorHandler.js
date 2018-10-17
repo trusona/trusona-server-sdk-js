fff42833462cb08272001739aa086d481c92d8fb
jd <juandiegogarita@gmail.com>
2018-10-17 16:41:23 -0600

    Updates the trusonafication error handler to parse the response and get the description properly

--- src/TrusonaficationErrorHandler.js
+++ src/TrusonaficationErrorHandler.js
@@ -1,14 +1,22 @@
 const GenericErrorHandler = require('./GenericErrorHandler')
+const NoIdentityDocumentError = require('./NoIdentityDocumentError')
 const TrusonaError = require('./TrusonaError')
 
 class TrusonaficationErrorHandler extends GenericErrorHandler {
 
-    static handleError(error){
-        super.handleError(error)
+    static handleError(response){
+        var parsedError = JSON.parse(response.error)
+        super.handleError(response)
         switch(error.statusCode) {
             case 424:
-                throw new TrusonaError(error.statusCode + " - " +
-                    error.message);
+                if(parsedError.error === "NO_DOCUMENTS"){
+                    throw new NoIdentityDocumentError(parsedError.description)
+                }
+                else{
+                    throw new TrusonaError(error.statusCode + " - " +
+                    parsedError.description);
+                }
+            
         }
     }
 }
