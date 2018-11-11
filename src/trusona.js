8b247978addb2810930eea17537f2fb7142b90d3
jd <juandiegogarita@gmail.com>
2018-11-10 22:45:58 -0600

    fixes polling for paired trucodes

--- src/trusona.js
+++ src/trusona.js
@@ -88,7 +88,7 @@ class Trusona {
       interval: 5000,
       timeout: timeout,
       shouldContinue(error, result) {
-        return error == null && result.status === `IN_PROGRESS`
+        return error === null && result.status === `IN_PROGRESS`
       }
     })
   }
@@ -162,7 +162,7 @@ class Trusona {
       interval: 5000,
       timeout: timeout,
       shouldContinue(error, result){
-        return error == null
+        return result === `undefined`
       }
     })
   }
